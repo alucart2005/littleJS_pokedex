2802192be39a4cad57e54e4c02d83f53eec3a25e
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-24 20:19:08 -0500

    feat: add launch.json

--- script/script.js
+++ script/script.js
@@ -1,5 +1,40 @@
 document.addEventListener('DOMContentLoaded', 
 function() {
+
+  const fetchPokemon = async (endpoint) => {
+    let data;
+    try {
+      const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      data = await response.json();
+    }catch(err) {
+      console.log(err);
+    }
+    return data.pokemon_species;
+ };
+ function orderNumber(str){
+  var mySubString = str.substring(
+    str.lastIndexOf("s/") + 2,
+    str.lastIndexOf("/")
+  );
+  return mySubString;
+ }
+ async function getPokemon(numero) {
+  let endpoint = `https://pokeapi.co/api/v2/generation/${numero}/`;
+  var container = document.getElementById("container");
+  container.innerHTML = "";
+  let pokemons = [];
+  pokemons = await fetchPokemon(endpoint);
+  for (let i = 0; i < pokemons.length; i++) {
+
+  }
+  // console.log(pokemons);
+ };
+  getPokemon(1);
   var geners = [
     "generation-1",
     "generation-2",
@@ -16,6 +51,4 @@ function() {
     <label for = ${geners[i]}  class="label-gens">${geners[i]}</label>`;
   }
   filters.innerHTML = gen;
-
-
 });
